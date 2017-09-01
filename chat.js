6ce2a3102f63c534efa49bc18f1b83454cd03ccf
ocket8888 <ocket8888@gmail.com>
2017-09-01 15:58:53 -0600

    client now just sends audio to server instead of playing it

--- chat.js
+++ chat.js
@@ -1,35 +1,46 @@
 var ws = new WebSocket("ws://superphage.us:6969/");
+
 var messages = document.getElementById('messages');
+
 ws.onmessage = function (event) {
 	console.log("Message Incoming: ", event);
-	var messages = document.getElementById('messages');
-	var message = document.createElement('div');
-	var messageContent = document.createElement('span');
-	messageContent.className = "messagecontent"; //freaking case-sensitivity is inconsistent smh tbh
-	message.className = "message metal";
-	var who = document.createElement('span');
-	who.className = "username";
-	var when = document.createElement('span');
-	when.className = "timestamp";
-	response = JSON.parse(event.data);
-	var timestamp = new Date(parseFloat(response.date)*1000.0)
-	var content = document.createTextNode(response.text);
-	var userContent = document.createTextNode(response.sender);
-	var timeContent = document.createTextNode('@ '+timestamp.getHours()+':'+timestamp.getMinutes()+' - ');
-	who.appendChild(userContent);
-	message.appendChild(who);
-	when.appendChild(timeContent);
-	message.appendChild(when);
-	messageContent.appendChild(content);
-	message.appendChild(messageContent);
-	messages.appendChild(message);
-	messages.scrollTop = messages.scrollHeight;
+	let response = JSON.parse(event.data);
+	if (response.type == 'textmsg') {
+		var messages = document.getElementById('messages');
+		var message = document.createElement('div');
+		var messageContent = document.createElement('span');
+		messageContent.className = "messagecontent"; //freaking case-sensitivity is inconsistent smh tbh
+		message.className = "message metal";
+		var who = document.createElement('span');
+		who.className = "username";
+		var when = document.createElement('span');
+		when.className = "timestamp";
+		var timestamp = new Date(parseFloat(response.date)*1000.0)
+		var content = document.createTextNode(response.text);
+		var userContent = document.createTextNode(response.sender);
+		var timeContent = document.createTextNode('@ '+timestamp.getHours()+':'+timestamp.getMinutes()+' - ');
+		who.appendChild(userContent);
+		message.appendChild(who);
+		when.appendChild(timeContent);
+		message.appendChild(when);
+		messageContent.appendChild(content);
+		message.appendChild(messageContent);
+		messages.appendChild(message);
+		messages.scrollTop = messages.scrollHeight;
+	}
+	else if(response.type == 'voip') {
+		console.log("do voip things");
+	}
+	else {
+		console.error("Malformed message received from server");
+	}
 };
+
 sendMessage = function () {
 	var msg = {
 		type: "textmsg",
 		text: document.getElementById("compose").value,
-	}
+	};
 	ws.send(JSON.stringify(msg));
 	document.getElementById("compose").value = "";
 };
@@ -48,6 +59,10 @@ setNickname = function () {
 	document.getElementById("nickname_modal").style.display = "none";
 };
 
+sendVoipPacket = function (pack) {
+	ws.send(pack);
+};
+
 //Check for voip capability
 if (navigator.mediaDevices) {
 	console.log("devices found; attempting voip");
@@ -59,23 +74,28 @@ if (navigator.mediaDevices) {
 		function(stream) {
 			var mediaRecorder = new MediaRecorder(stream);
 
-			mediaRecorder.start(1000);
+			mediaRecorder.start(50);
 
 			console.log("Started recording sound. May god have mercy on my soul.");
 
 			mediaRecorder.ondataavailable = function(event) {
 				console.log("we got us some audio!");
-				audio = document.getElementById('speaker');
+				
+				//TODO - get audio playback working.
+				/*audio = document.getElementById('speaker');
 				audio.src = URL.createObjectURL(event.data);
 				audio.load();
-				audio.play();
+				audio.play();*/
+
 				console.log(event);
 				chunks.push(event.data);
+				sendVoipPacket(event.data);
 			};
 
 			mediaRecorder.onerror = function(event) {
 				console.log("Whoops. An accident.");
 				console.log(event);
+				mediaRecorder.stop();
 			};
 
 		}).catch(function(error) {
