8859d2bf6fceabaab1011d1baaa8b011c9bd1d85
ocket8888 <ocket8888@gmail.com>
2017-09-01 18:01:48 -0600

    now treats binary data differently than strings from socket

--- chat.js
+++ chat.js
@@ -1,38 +1,51 @@
 var ws = new WebSocket("ws://superphage.us:6969/");
 
 var messages = document.getElementById('messages');
+var speaker = document.getElementById('speaker');
 
 ws.onmessage = function (event) {
 	console.log("Message Incoming: ", event);
-	let response = JSON.parse(event.data);
-	if (response.type == 'textmsg') {
-		var messages = document.getElementById('messages');
-		var message = document.createElement('div');
-		var messageContent = document.createElement('span');
-		messageContent.className = "messagecontent"; //freaking case-sensitivity is inconsistent smh tbh
-		message.className = "message metal";
-		var who = document.createElement('span');
-		who.className = "username";
-		var when = document.createElement('span');
-		when.className = "timestamp";
-		var timestamp = new Date(parseFloat(response.date)*1000.0)
-		var content = document.createTextNode(response.text);
-		var userContent = document.createTextNode(response.sender);
-		var timeContent = document.createTextNode('@ '+timestamp.getHours()+':'+timestamp.getMinutes()+' - ');
-		who.appendChild(userContent);
-		message.appendChild(who);
-		when.appendChild(timeContent);
-		message.appendChild(when);
-		messageContent.appendChild(content);
-		message.appendChild(messageContent);
-		messages.appendChild(message);
-		messages.scrollTop = messages.scrollHeight;
+
+	if (event.data instanceof Blob) {
+		var speaker = document.getElementById('speaker');
+		console.log("Got a voip packet");
+		var src = URL.createObjectURL(event.data);
+		speaker.src = src;
+		speaker.load();
+		speaker.play().then(() => {console.log("playback success!");}, () => {console.log("playback failure...");});
 	}
-	else if(response.type == 'voip') {
-		console.log("do voip things");
+	else if (event.data instanceof String) {
+		let response = JSON.parse(event.data);
+		console.log(response);
+		if (response.type == 'textmsg') {
+			var messages = document.getElementById('messages');
+			var message = document.createElement('div');
+			var messageContent = document.createElement('span');
+			messageContent.className = "messagecontent"; //freaking case-sensitivity is inconsistent smh tbh
+			message.className = "message metal";
+			var who = document.createElement('span');
+			who.className = "username";
+			var when = document.createElement('span');
+			when.className = "timestamp";
+			var timestamp = new Date(parseFloat(response.date)*1000.0)
+			var content = document.createTextNode(response.text);
+			var userContent = document.createTextNode(response.sender);
+			var timeContent = document.createTextNode('@ '+timestamp.getHours()+':'+timestamp.getMinutes()+' - ');
+			who.appendChild(userContent);
+			message.appendChild(who);
+			when.appendChild(timeContent);
+			message.appendChild(when);
+			messageContent.appendChild(content);
+			message.appendChild(messageContent);
+			messages.appendChild(message);
+			messages.scrollTop = messages.scrollHeight;
+		}
+		else {
+			console.error("Malformed message received from server");
+		}
 	}
 	else {
-		console.error("Malformed message received from server");
+		console.log("Malformed data packet received from server");
 	}
 };
 
@@ -72,31 +85,17 @@ if (navigator.mediaDevices) {
 
 	navigator.mediaDevices.getUserMedia(constraints).then(
 		function(stream) {
-			var mediaRecorder = new MediaRecorder(stream);
+			var context = new AudioContext();
+			
+			var source = context.createMediaStreamSource(stream);
+			var proc = context.createScriptProcessor(2048, 2, 2);
+			source.connect(proc);
 
-			mediaRecorder.start(50);
-
-			console.log("Started recording sound. May god have mercy on my soul.");
-
-			mediaRecorder.ondataavailable = function(event) {
-				console.log("we got us some audio!");
-				
-				//TODO - get audio playback working.
-				/*audio = document.getElementById('speaker');
-				audio.src = URL.createObjectURL(event.data);
-				audio.load();
-				audio.play();*/
-
-				console.log(event);
-				chunks.push(event.data);
-				sendVoipPacket(event.data);
-			};
-
-			mediaRecorder.onerror = function(event) {
-				console.log("Whoops. An accident.");
-				console.log(event);
-				mediaRecorder.stop();
-			};
+			proc.onaudioprocess = function (e) {
+				console.log("collecting audio data...")
+				var audio_data = e.inputBuffer.getChannelData(0)|| new Float32Array(2048);
+				sendVoipPacket(audio_data);
+			}
 
 		}).catch(function(error) {
 			console.log("Something wicked happened... : "+error);
